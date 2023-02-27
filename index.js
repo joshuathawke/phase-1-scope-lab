91bb256551c0586f4d9b514de226242eaa1a560e
Josh Haawke <joshuathawke@gmail.com>
2023-02-27 13:15:51 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(customer) {
+    bestCustomer = "not bob"
+}
+ function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+ }
+ function changeLeastFavoriteCustomer(){
+
+    const leastFavoriteCustomer = "somebody"
+
+    leastFavoriteCustomer = customerName;
+}
